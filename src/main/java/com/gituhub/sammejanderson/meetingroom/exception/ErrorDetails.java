dcb70b1ad95b130a12aea0b34bb411e7620b51c2
SammeJaderson <samme.san@gmail.com>
2020-11-06 11:38:05 -0300

    implemtent exceptions

--- src/main/java/com/gituhub/sammejanderson/meetingroom/exception/ErrorDetails.java
+++ src/main/java/com/gituhub/sammejanderson/meetingroom/exception/ErrorDetails.java
@@ -1,4 +1,16 @@
 package com.gituhub.sammejanderson.meetingroom.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
 public class ErrorDetails {
+    private Date timestamp; //data e hora do erro
+    private String messager; //menssagem do erro
+    private String details; //detalhes do erro
+
+
 }
--- src/main/java/com/gituhub/sammejanderson/meetingroom/model/Room.java
+++ src/main/java/com/gituhub/sammejanderson/meetingroom/model/Room.java
@@ -34,8 +34,6 @@ public class Room {
     private String endHour;
 
 
-
-
 }
 
 
